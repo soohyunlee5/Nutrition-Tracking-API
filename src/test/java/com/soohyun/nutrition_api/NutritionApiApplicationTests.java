02a76629884d5a0a6c3f2b621ea1aa85830365de
soohyunlee5 <soohyunpaullee@gmail.com>
2026-06-12 15:26:13 -0600

    Initial project setup

---
+++ src/test/java/com/soohyun/nutrition_api/NutritionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soohyun.nutrition_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
